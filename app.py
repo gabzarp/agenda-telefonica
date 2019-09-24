b3a4fae3169867bf009d8cf791da42e34cd3e40a
Gabriel Zarpelon <zarpelon091098@gmail.com>
2019-09-23 23:49:11 -0300

    List page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, escape, request, json, jsonify
+from flask import Flask, escape, request, json, jsonify, render_template
 from flask_pymongo import PyMongo
 import datetime
 
@@ -8,24 +8,39 @@ app.config["MONGO_URI"] = "mongodb://localhost:27017/contacts"
 mongo = PyMongo(app)
 
 
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/contacts')
+def contacts():
+    return render_template("contacts.html", contacts=mongo.db.contacts.find())
+
+@app.route('/create-contact')
+def createContact():
+    return render_template("create-contact.html")
+
 @app.route('/api/getContatos', methods=["GET"])
 def getAllContacts():
     contacts = []
     for i in mongo.db.contacts.find():
-        contacts.append({'id': str(i['_id']), 'name': i['name'] })
+        contacts.append({'id': str(i['_id']), 'name': i['name'], 'phones': i['phones'], 'addresses': i['addresses'] })
     return jsonify(contacts), 200
 
 
 @app.route('/api/getContatoID/<ObjectId:id>', methods=["GET"])
 def getContactById(id):
     contact = mongo.db.contacts.find_one_or_404({'_id': id })
-    return jsonify({'id': str(contact['_id']) , 'name': contact['name'] }), 200
+    return jsonify({'id': str(contact['_id']), 'name': contact['name'] }), 200
 
 
 @app.route('/api/getContatoNome/<name>', methods=["GET"])
 def getContactByName(name):
-    contact = mongo.db.contacts.find_one_or_404({'name': {'$regex': name} })
-    return jsonify({'id': str(contact['_id']) , 'name': contact['name'] }), 200
+    contacts = []
+    for i in mongo.db.contacts.find({ 'name': {'$regex': name }} ):
+        contacts.append({'id': str(i['_id']), 'name': i['name'], 'phones': i['phones'], 'addresses': i['addresses']})
+    return jsonify(contacts), 200
 
 
 @app.route('/api/getContatoAniversario/<month>', methods=["GET"])
